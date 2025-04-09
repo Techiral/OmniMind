1ef8c830d38a414911f55fa601b29a842e7aeb1d
Techiral <techiralthefuture@gmail.com>
2025-04-09 16:40:51 +0530

    Published to test-pypi

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="omnimind",
+    version="0.1.0",
+    author="Lakshya Gupta",
+    author_email="techiralthefuture@gmail.com",
+    description="A plug-and-play Python library for effortless MCP server integration, powered by Google Gemini",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Techiral/OmniMind",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
